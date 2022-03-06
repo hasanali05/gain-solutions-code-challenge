7eda974a042b2dc6774f7ce5748204b40dde2acd
Md Hasan Ali <hasan.lynkto@gmail.com>
2022-03-06 18:26:08 +0600

    vue basic setup integration - done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+
+import Segments from './components/SegmentsComponent.vue'
+import Subscription from './components/SubscriptionComponent.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        Segments: Segments,
+        Subscription: Subscription,
+    },
+});
